e51fe9c0d07ed246b8a389196817807a624b619e
Min-ho Kim <minmin0416@naver.com>
2017-12-02 21:57:26 +0900

    Add comment

--- modules/base62.js
+++ modules/base62.js
@@ -1,6 +1,8 @@
 BASE = 62;
+// 62진수로 표현하기 위해 필요한 table
 const table = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
+// DB id값을 62진수로 전환
 exports.toBase62 = function(id, base62Alg, callback){
     var arr = base62Alg;
     do{
@@ -12,6 +14,7 @@ exports.toBase62 = function(id, base62Alg, callback){
     }while(id)
 }
 
+// 62진수 문자열을 id 값으로(10진수) 바꿈
 exports.fromBase62 = function(shorturl, callback){
     var result = 0;
     var pos = 0;
